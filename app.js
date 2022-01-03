2aa604198e0e6d4ae3bfdcc77cec7817b5d56f09
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2022-01-03 16:47:03 -0500

    initial commit, set up basic files

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3333;
+
+const cors = require('cors');
+
+const express = require('express');;
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use(cors());
+
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server is running at: http://${hostname}:${port}`);
+})
