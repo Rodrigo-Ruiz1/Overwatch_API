ab694e130db953760346affd317ec9e6fd5cdf23
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2022-01-07 01:05:56 -0500

    updates heroes routes endpoints

--- routes/heroes.js
+++ routes/heroes.js
@@ -6,13 +6,13 @@ const HeroesModel = require('../models/HeroesModel');
 router.get('/', async(req, res) => {
     const response = await HeroesModel.getAllHeroes();
     if (response !== undefined) {
-        res.json(response).status(200);
+        res.json(response[0]).status(200);
     } else {
         res.status(404);
     }
 });
 
-router.get('/hero/:hero_name', async(req, res) => {
+router.get('/:hero_name', async(req, res) => {
     const {hero_name} = req.params;
     const response = await HeroesModel.getHeroByHeroName(hero_name);
     if (response !== undefined) {
@@ -22,13 +22,13 @@ router.get('/hero/:hero_name', async(req, res) => {
     }
 });
 
-router.get('/hero/id/:hero_id', async(req, res) => {
+router.get('/id/:hero_id', async(req, res) => {
     const {hero_id} = req.params;
     const response = await HeroesModel.getHeroByHeroId(hero_id);
     res.json(response[0]).status(200);
 });
 
-router.get('/:role', async(req, res) => {
+router.get('/role/:role', async(req, res) => {
     const {role} = req.params;
     const response = await HeroesModel.getHeroesByRole(role);
     if (response !== undefined) {
