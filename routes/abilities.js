33acc8aa8d5b70de2a4e5f1bfe4bb4994a473c31
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2022-01-14 21:10:38 -0500

    updated routes to return error if response is empty

--- routes/abilities.js
+++ routes/abilities.js
@@ -1,34 +1,34 @@
-'use state';
+'use strict';
 const express = require('express');
 const router = express.Router();
 const AbilitiesModel = require('../models/AbilitiesModel');
 
 router.get('/', async(req, res) => {
     const response = await AbilitiesModel.getAllAbilities();
-    if (response !== undefined) {
+    if (response.length > 0) {
         res.json(response[0]).status(200)
     } else {
-        res.status(404);
+        res.status(404).json({error : 'No Results Found'});
     }
 });
 
 router.get('/id/:ability_id', async(req, res) => {
     const {ability_id} = req.params;
     const response = await AbilitiesModel.getAbilityById(ability_id);
-    if (response !== undefined) {
+    if (response.length > 0) {
         res.json(response[0]).status(200);
     } else {
-        res.status(404);
+        res.status(404).json({error : 'No Results Found'});
     }
 });
 
 router.get('/:ability_name', async(req, res) => {
     const {ability_name} = req.params;
     const response = await AbilitiesModel.getAbilityByName(ability_name);
-    if (response !== undefined) {
+    if (response.length > 0) {
         res.json(response[0]).status(200);
     } else {
-        res.status(404);
+        res.status(404).json({error : 'No Results Found'});
     }
 });
 
--- routes/heroes.js
+++ routes/heroes.js
@@ -5,36 +5,36 @@ const HeroesModel = require('../models/HeroesModel');
 
 router.get('/', async(req, res) => {
     const response = await HeroesModel.getAllHeroes();
-    if (response !== undefined) {
-        res.json(response[0]).status(200);
-    } else {
-        res.status(404);
-    }
+    res.json(response[0]).status(200);
 });
 
 router.get('/:hero_name', async(req, res) => {
     const {hero_name} = req.params;
     const response = await HeroesModel.getHeroByHeroName(hero_name);
-    if (response !== undefined) {
+    if (response.length > 0) {
         res.json(response[0]).status(200);
     } else {
-        res.status(404);
+        res.status(404).json({error : 'No Results Found'})
     }
 });
 
 router.get('/id/:hero_id', async(req, res) => {
     const {hero_id} = req.params;
     const response = await HeroesModel.getHeroByHeroId(hero_id);
-    res.json(response[0]).status(200);
+    if (response.length > 0) {
+        res.json(response[0]).status(200);
+    } else {
+        res.status(404).json({error : 'No Results Found'})
+    }
 });
 
 router.get('/role/:role', async(req, res) => {
     const {role} = req.params;
     const response = await HeroesModel.getHeroesByRole(role);
-    if (response !== undefined) {
+    if (response.length > 0) {
         res.json(response).status(200)
     } else {
-        res.status(404);
+        res.status(404).json({error : 'No Results Found'});
     }
 });
 
