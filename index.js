03e2fd6f0a1c891ce81fce59dadbd079ac881ea5
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2022-01-28 18:14:13 -0500

    testing for heroku deployment

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 'use strict'
 
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3333;
+// const http = require('http');
+// const hostname = '127.0.0.1';
+// const port = 3333;
 
-const cors = require('cors');
+// const cors = require('cors');
 
 const express = require('express');;
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.static('public'));
+// app.use(express.static('public'));
+// app.use(express.json());
+// app.use(express.static('public'));
 
-app.use(cors());
+// app.use(cors());
 
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(port, hostname, () => {
-    console.log(`Server is running at: http://${hostname}:${port}`);
-})
+app.listen(5000);
 
 const rootController = require('./routes/index');
 const heroesController = require('./routes/heroes');
